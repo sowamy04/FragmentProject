9293d4b38e0bba93aca433fbe68f3ff41561b313
sowamy04 <amysow04@gmail.com>
2019-12-11 10:32:05 +0000

    first commit

---
+++ app/src/main/java/com/amy/fragmentproject/ui/main/MainFragment.java
@@ -0,0 +1,63 @@
+package com.amy.fragmentproject.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.amy.fragmentproject.R;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+    public Button bouton;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.main_fragment, container, false);
+        bouton = v.findViewById(R.id.button);
+        if (bouton == null) {
+            Log.i("Bouton", "Le bouton est null");
+        }
+        else {
+            Log.i("Bouton", "Le bouton est non null");
+        }
+
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Click", Toast.LENGTH_SHORT).show();
+                Log.i("Fragment", "Exercice sur les fragments");
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+
+
+
+    }
+
+}
